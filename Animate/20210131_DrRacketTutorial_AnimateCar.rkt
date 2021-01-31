773c48d9705cc6850d0b25d83dd8ac7fc37be3e8
Pyline Tangsuvanich <pylinet@gmail.com>
2021-01-31 18:17:37 -0500

    Updated colors

--- Animate/20210131_DrRacketTutorial_AnimateCar.rkt
+++ Animate/20210131_DrRacketTutorial_AnimateCar.rkt
@@ -11,45 +11,75 @@
 (define WHEEL-DISTANCE (* WHEEL-RADIUS 5))
 
 (define WHEEL
-  (circle WHEEL-RADIUS "solid" "black"))
+  (circle WHEEL-RADIUS "solid" "saddle brown"))
 (define SPACE
-  (rectangle WHEEL-RADIUS 5 "solid" "white"))
+  (rectangle 8 5 "solid" "coral"))
 (define BOTH-WHEELS
   (beside WHEEL SPACE WHEEL))
 
+; Create clouds
+(define CLOUD
+  (underlay/offset (circle 15 "solid" "Floral white") 16 10 (circle 12 "solid" "Floral white")))
+
 ; Create car body
 (define CAR-BODY
-  (rectangle (* 4 WHEEL-RADIUS)(* 4 WHEEL-RADIUS) "solid" "red"))
+  (rectangle (* 4 WHEEL-RADIUS)(* 4 WHEEL-RADIUS) "solid" "coral"))
 (define CAR-BODY-SIDE
-  (rectangle (* 2 WHEEL-RADIUS)(* 2 WHEEL-RADIUS) "solid" "red"))
+  (rectangle (* 2 WHEEL-RADIUS)(* 2 WHEEL-RADIUS) "solid" "coral"))
 (define TOTAL-CAR-BODY
   (beside/align "bottom" CAR-BODY-SIDE CAR-BODY CAR-BODY-SIDE))
 
 ; Combine the wheels and car body
 (define COMPLETE-CAR
-  (underlay/align/offset "center" "bottom" TOTAL-CAR 0 WHEEL-RADIUS BOTH-WHEELS))
+  (underlay/align/offset "center" "bottom" TOTAL-CAR-BODY 0 WHEEL-RADIUS BOTH-WHEELS))
+
+; Create houses
+(define HOUSE-ROOF
+  (regular-polygon 36 3 "solid" "sienna"))
+(define HOUSE-BODY
+  (rectangle 30 30 "solid" "peru"))
+(define TOTAL-HOUSE
+  (overlay/xy HOUSE-ROOF 3 30 HOUSE-BODY))
 
 
 ; Create trees
-(define tree
-  (underlay/xy (circle 10 "solid" "green")
-               9 15
-               (rectangle 2 20 "solid" "brown")))
+(define TREE
+  (underlay/xy (circle 12 "solid" "seagreen") 10 24 (rectangle 3 20 "solid" "brown")))
+
+; Create background scene
+(define SCENE
+  (above/align "middle" (rectangle WIDTH-OF-WORLD 80 "solid" "Light Cyan") (rectangle WIDTH-OF-WORLD 90 "solid" "Medium Goldenrod") (rectangle WIDTH-OF-WORLD 30 "solid" "Gainsboro")))
 
 ; Create background for which car and trees will live
+(define BACKGROUND-1
+  (underlay/xy SCENE 50 70 (underlay/xy TOTAL-HOUSE 50 20 (underlay/xy TREE 40 -10 TREE))))
+
 (define BACKGROUND
-  (overlay/align "center" "bottom" tree (rectangle WIDTH-OF-WORLD WIDTH-OF-WORLD "solid" "white")))
-
-
+  (underlay/xy BACKGROUND-1 20 20 CLOUD))
 
 ; WorldState -> Image
 ; places the car into the BACKGROUND scene,
 ; according to the given world state
-(define Y-CAR 50)
+(define Y-CAR 170)
 (define (render cw) (place-image COMPLETE-CAR cw Y-CAR BACKGROUND))
-
 
 
 ; WorldState -> WorldState
 ; moves the car by 3 pixels for every clock tick
 (define (tock cw) (+ cw 3))
+
+
+(define (main ws)
+   (big-bang ws
+     [on-tick tock]
+     [on-mouse hyper]
+     [to-draw render]))
+
+
+; WorldState Number Number String -> WorldState
+; places the car at x-mouse
+; if the given me is "button-down" 
+(define (hyper x-position-of-car x-mouse y-mouse me)
+  (cond
+    [(string=? "button-down" me) x-mouse]
+    [else x-position-of-car]))
